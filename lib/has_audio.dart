c0ea12a74a78fa6304f5f4c1b2d2cfed5d814e2b
Ahsan Raza <ahsanrazapk92@mail.com>
2024-01-28 02:09:44 +0500

    add comments

--- lib/has_audio.dart
+++ lib/has_audio.dart
@@ -1,8 +1,23 @@
 import 'package:has_audio/has_audio_method_channel.dart';
 
+/// A Flutter plugin for checking if a video file contains an audio track.
+///
+/// This plugin provides a simple method to quickly determine the presence of
+/// audio in a video file on both Android and iOS platforms.
 class HasAudio {
   static final _instance = MethodChannelHasAudio();
 
+
+
+  /// Checks if the provided video file contains an audio track.
+  ///
+  /// This method takes the path to a video file and checks if the file
+  /// contains an audio track. It returns `true` if audio is present,
+  /// otherwise `false`.
+  ///
+  /// [videoPath] The path to the video file.
+  ///
+  /// Returns a [Future<bool>] indicating the presence of audio.
   static Future<bool> check(String videoPath) {
     return _instance.check(videoPath);
   }
--- lib/has_audio_method_channel.dart
+++ lib/has_audio_method_channel.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/services.dart';
 import 'has_audio_platform_interface.dart';
 
+/// A method channel implementation of the [HasAudio] plugin.
+///
+/// This class uses method channels to interact with the platform-specific
+/// implementation for checking audio in a video file.
 class MethodChannelHasAudio extends HasAudioInterface {
   static const _channel = MethodChannel('com.falcon.has_audio');
 
+  /// Checks if the provided video file contains an audio track.
+  ///
+  /// This method uses platform channels to invoke native code for checking
+  /// the presence of audio in the video file specified by [videoPath].
   @override
   Future<bool> check(String videoPath) async {
     try {
-      final bool hasAudio =
-          await _channel.invokeMethod('hasAudio', {'videoPath': videoPath});
+      final bool hasAudio = await _channel.invokeMethod('hasAudio', {'videoPath': videoPath});
       return hasAudio;
     } on PlatformException catch (_) {
       return false;
